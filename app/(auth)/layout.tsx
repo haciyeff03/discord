e4b290a269b88573c15d05279535324fd382f5a9
Your Name <haciyevqurban97@gmail,com>
2023-10-01 00:27:34 +0400

    server create,image upload

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+
+const AuthLayout=({children}:{children:React.ReactNode})=>{
+    return(
+        <div
+        className="h-full bg-gray-200 flex justify-center items-center">
+            {children}
+            </div>
+
+            
+    )
+}
+export default AuthLayout;
---
+++ components/file-upload.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { strict } from "assert";
+import { string } from "zod";
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css"
+interface FileUploadProps {
+    onChange:(url?: string)=> void;
+    value:string;
+    endpoint:"messageFile" | "serverImage";
+}
+export const FileUpload= ({
+    onChange,
+    value,
+    endpoint
+}: FileUploadProps)=> {
+    return (
+        <UploadDropzone
+        endpoint={endpoint}
+        onClientUploadComplete={(res)=> {
+            onChange(res?.[0].fileUrl);
+        }}/>
+
+      
+    )
+}
